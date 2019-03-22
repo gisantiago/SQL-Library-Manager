9f23cc990eb8a91793e3973cae32b45b7f2abf47
Gabriel Santiago <gisantiago@live.com>
2019-03-22 14:49:45 -0400

    Update scripts.js

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -6,27 +6,27 @@
 
 $(document).ready(function(){
 
-    $('#table').after('<p id="empty"></p>');
-    $('#empty').after('<div id="nav"></div>');
-    var rowsShown = 6;
-    var rowsTotal = $('#table tbody tr').length;
-    var numPages = rowsTotal/rowsShown;
-    for(i = 0;i < numPages;i++) {
-        var pageNum = i + 1;
-        $('#nav').append('<a href="#" rel="'+i+'">'+pageNum+'</a> ');
-    }
-    $('#table tbody tr').hide();
-    $('#table tbody tr').slice(0, rowsShown).show();
-    $('#nav a:first').addClass('active');
-    $('#nav a').bind('click', function(){
+    // $('#table').after('<p id="empty"></p>');
+    // $('#empty').after('<div id="nav"></div>');
+    // var rowsShown = 6;
+    // var rowsTotal = $('#table tbody tr').length;
+    // var numPages = rowsTotal/rowsShown;
+    // for(i = 0;i < numPages;i++) {
+    //     var pageNum = i + 1;
+    //     $('#nav').append('<a href="#" rel="'+i+'">'+pageNum+'</a> ');
+    // }
+    // $('#table tbody tr').hide();
+    // $('#table tbody tr').slice(0, rowsShown).show();
+    // $('#nav a:first').addClass('active');
+    // $('#nav a').bind('click', function(){
 
-        $('#nav a').removeClass('active');
-        $(this).addClass('active');
-        var currPage = $(this).attr('rel');
-        var startItem = currPage * rowsShown;
-        var endItem = startItem + rowsShown;
-        $('#table tbody tr').css('opacity','0.0').hide().slice(startItem, endItem).
-        css('display','table-row').animate({opacity:1}, 300);
-    });
+    //     $('#nav a').removeClass('active');
+    //     $(this).addClass('active');
+    //     var currPage = $(this).attr('rel');
+    //     var startItem = currPage * rowsShown;
+    //     var endItem = startItem + rowsShown;
+    //     $('#table tbody tr').css('opacity','0.0').hide().slice(startItem, endItem).
+    //     css('display','table-row').animate({opacity:1}, 300);
+    // });
 
 });
